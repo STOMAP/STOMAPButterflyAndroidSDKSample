89d693d4b61a14cb51c81da41890d0b093627604
SCL <achi001@gmail.com>
2019-04-22 13:05:04 +0800

    April 22, 2019 #Updated SDK
    Release Note:
    1. Updated SDK v2019.04.22-r0

--- app/src/main/java/com/stomap/stomapbutterflysdksample/MainActivity.java
+++ app/src/main/java/com/stomap/stomapbutterflysdksample/MainActivity.java
@@ -2,17 +2,22 @@ package com.stomap.stomapbutterflysdksample;
 
 import android.Manifest;
 import android.annotation.TargetApi;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.Toast;
+import android.support.v7.app.AlertDialog;
 import com.stomap.stomapbutterflysdk.STOMAPButterflySDK;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 1001;
     private Intent mSTOMAPServiceIntent;
 
     @TargetApi(Build.VERSION_CODES.M)
@@ -21,9 +26,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Request for Location Service Permission
-        if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            this.requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        if (Build.VERSION.SDK_INT >= 23) {
+            // Check Permission APIs
+            requestPermission();
         }
 
         // STOMAPButterflyService Intent Setup
@@ -34,5 +39,68 @@ public class MainActivity extends AppCompatActivity {
 
         // STOMAPButterflyService Intent Start
         this.startService(mSTOMAPServiceIntent);
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void requestPermission() {
+        List<String> permissionsNeeded = new ArrayList<String>();
+
+        final List<String> permissionsList = new ArrayList<String>();
+
+        //ACCESS_FINE_LOCATION
+        if (!addPermission(permissionsList, Manifest.permission.ACCESS_FINE_LOCATION))
+            permissionsNeeded.add("Show Location");
+
+        //BLUETOOTH_ADMIN
+        if (!addPermission(permissionsList, Manifest.permission.BLUETOOTH_ADMIN))
+            permissionsNeeded.add("enable bluetooth admin");
+
+        //BLUETOOTH
+        if (!addPermission(permissionsList, Manifest.permission.BLUETOOTH))
+            permissionsNeeded.add("enable bluetooth");
+
+        if (permissionsList.size() > 0) {
+            if (permissionsNeeded.size() > 0) {
+                // Need Rationale
+                String message = "App need access to " + permissionsNeeded.get(0);
+                for (int i = 1; i < permissionsNeeded.size(); i++)
+                    message = message + ", " + permissionsNeeded.get(i);
+
+                showMessageOKCancel(message,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                requestPermissions(permissionsList.toArray(new String[permissionsList.size()]),
+                                        REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+                            }
+                        });
+                return;
+            }
+            requestPermissions(permissionsList.toArray(new String[permissionsList.size()]),
+                    REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+            return;
+        }
+        Toast.makeText(MainActivity.this, "No new Permission Required- Launching App .You are Awesome!!", Toast.LENGTH_SHORT).show();
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private boolean addPermission(List<String> permissionsList, String permission) {
+
+        if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+            permissionsList.add(permission);
+            // Check for Rationale Option
+            if (!shouldShowRequestPermissionRationale(permission))
+                return false;
+        }
+        return true;
     }
 }
